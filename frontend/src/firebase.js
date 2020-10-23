8308b786c682693b4e4e7fdf6679c7225c867296
Ameer Hussain <50126669+ameer117@users.noreply.github.com>
2020-10-23 16:37:55 -0700

    Add files via upload

---
+++ frontend/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBpV8LAwkgz65VOmBH1PyermYrXyCLIGSs",
+    authDomain: "whatsapp-80ab0.firebaseapp.com",
+    databaseURL: "https://whatsapp-80ab0.firebaseio.com",
+    projectId: "whatsapp-80ab0",
+    storageBucket: "whatsapp-80ab0.appspot.com",
+    messagingSenderId: "361047722442",
+    appId: "1:361047722442:web:1ea47263000a88b6e42733"
+  };
